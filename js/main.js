d7e2abbf3e94cf14cea81339b31dfdeb4b646ddc
LucaIapadre98 <luca.iap.1998@gmail.com>
2025-04-07 16:46:44 +0200

    aggiustamenti

--- js/main.js
+++ js/main.js
@@ -9,14 +9,14 @@ console.log(userWord);
 
 function isPalindroma(userWord){
     for (i = 0; i < userWord.length; i++) {
-        const isPalindroma = userWord.split().reverse().join("");
+        const isPalindroma = userWord.split("").reverse().join("");
         console.log(isPalindroma);
         if (userWord === isPalindroma){
             return true;
         }
-    }
-    if (userWord !== isPalindroma){
-        return false;
+        if (userWord !== isPalindroma){
+            return false;
+        }
     }
 }
 console.log(isPalindroma(userWord));
